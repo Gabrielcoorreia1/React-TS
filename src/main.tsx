e5c4cbcfb45fea33aeba4b154db30f3e47e3e0a8
Gabriel Correia <gabrielmasterprimeiro@gmail.com>
2024-06-15 20:57:54 -0300

    ae

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-const router  = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>,
-    children: [
-      {
-        
-      }
-    ]
-  }
-])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
