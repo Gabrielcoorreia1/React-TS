c4c71154b55f7fb193857cd76e65c199eddcc190
Gabriel Correia <gabrielmasterprimeiro@gmail.com>
2024-06-15 21:08:00 -0300

    ads

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Trabalho-4"
+  base: "/Trabalho-4",
+
+  publicDir: 'public',
+
 })
